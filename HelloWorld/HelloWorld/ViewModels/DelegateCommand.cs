25012a1ab17fc07e6d68a4566c26391e9f993efd
saburah-samples <saburah2@gmail.com>
2014-06-14 01:35:10 +0400

    HelloWorld - add MVVM base classes

---
+++ HelloWorld/HelloWorld/ViewModels/DelegateCommand.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HelloWorld.ViewModels
+{
+    public class DelegateCommand : ICommand
+    {
+        private Predicate<object> canExecute;
+        private Action<object> execute;
+
+        public DelegateCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            Debug.Assert(execute != null, "Argument 'execute' is null");
+
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute == null ? true : this.canExecute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            this.execute(parameter);
+        }
+    }
+}
---
+++ HelloWorld/HelloWorld/ViewModels/ViewModelBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld.ViewModels
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
