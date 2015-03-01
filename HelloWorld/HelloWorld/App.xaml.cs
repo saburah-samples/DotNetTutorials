f7e09df742d2f8383467c84df4fe6a3ba492b23a
Denis_Vakhrushev <saburah2@gmail.com>
2015-03-01 19:28:57 +0400

    HelloWorld - added AppServices

--- HelloWorld/HelloWorld/App.xaml.cs
+++ HelloWorld/HelloWorld/App.xaml.cs
@@ -13,5 +13,30 @@ namespace HelloWorld
     /// </summary>
     public partial class App : Application
     {
+        private AppServices appServices = new AppServices();
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            appServices.Startup(this, e.Args);
+        }
+        
+        protected override void OnActivated(EventArgs e)
+        {
+            base.OnActivated(e);
+            appServices.Resume(e);
+        }
+
+        protected override void OnDeactivated(EventArgs e)
+        {
+            appServices.Suspend(e);
+            base.OnDeactivated(e);
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            appServices.Shutdown(e.ApplicationExitCode);
+            base.OnExit(e);
+        }
     }
 }
---
+++ HelloWorld/HelloWorld/AppServices.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class AppServices
+    {
+        public AppServices()
+        {
+            
+        }
+
+        public void Startup(App app, string[] args)
+        {
+            WriteDebug("app services are starting up...");
+            App = app;
+            if (MainWindow == null)
+                WriteTrace("MainWindow = null");
+        }
+
+        public void Resume(EventArgs e)
+        {
+            WriteDebug("app services are resuming...");
+            if (MainWindow != null)
+                WriteTrace("MainWindow != null");
+        }
+
+        public void Suspend(EventArgs e)
+        {
+            WriteDebug("app services are suspending...");
+            if (MainWindow != null)
+                WriteTrace("MainWindow != null");
+        }
+
+        public void Shutdown(int exitCode)
+        {
+            WriteDebug("app services are shutting down...");
+            if (MainWindow == null)
+                WriteTrace("MainWindow == null");
+            App = null;
+        }
+
+        private void WriteDebug(string message)
+        {
+            System.Diagnostics.Debug.WriteLine(message);
+        }
+
+        private void WriteTrace(string message)
+        {
+            System.Diagnostics.Trace.WriteLine(message);
+        }
+
+        public App App { get; private set; }
+
+        public MainWindow MainWindow
+        {
+            get
+            {
+                if (App != null && App.MainWindow != null)
+                    return App.MainWindow as MainWindow;
+                else
+                    return null;
+            }
+        }
+    }
+}
