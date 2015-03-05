0680d8259c3bae6dceb8d62d2078030f4d38e397
Denis_Vakhrushev <saburah2@gmail.com>
2015-03-05 20:03:38 +0400

    HelloWorld.DragDropExample - Adding a New User Control to the Project

---
+++ HelloWorld/DragDropExample/Circle.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DragDropExample
+{
+    /// <summary>
+    /// Interaction logic for Circle.xaml
+    /// </summary>
+    public partial class Circle : UserControl
+    {
+        private Brush previousFill;
+        public Circle()
+        {
+            InitializeComponent();
+        }
+
+        public Circle(Circle c)
+        {
+            InitializeComponent();
+            this.circleUI.Height = c.circleUI.Height;
+            this.circleUI.Width = c.circleUI.Height;
+            this.circleUI.Fill = c.circleUI.Fill;
+        }
+    }
+}
