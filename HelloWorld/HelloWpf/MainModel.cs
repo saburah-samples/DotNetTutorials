969a3ea393a71f08283f77b5c17944cdb386990b
Denis_Vakhrushev <saburah2@gmail.com>
2015-03-09 17:55:44 +0400

    HelloWorld.HelloWpf - device control panel, inital commit

---
+++ HelloWorld/HelloWpf/MainModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace HelloWpf
+{
+    public class MainDevice : INotifyDeviceStateChanged
+    {
+        public void ExecuteCommand(string command, params string[] args)
+        {
+            Command = command;
+            NotifyChanged();
+        }
+
+        protected void NotifyChanged()
+        {
+            NotifyStateChanged(State, Command);
+        }
+
+        private void NotifyStateChanged(string state, string command)
+        {
+            if (OnStateChanged != null)
+                OnStateChanged(this, new DeviceStateChangedEventArgs(state, command));
+        }
+
+        public string Command { get; private set; }
+        public string State { get; private set; }
+
+        public event DeviceStateChangedEventHandler OnStateChanged;
+    }
+
+    public interface INotifyDeviceStateChanged
+    {
+        event DeviceStateChangedEventHandler OnStateChanged;
+    }
+
+    public class DeviceStateChangedEventArgs : EventArgs
+    {
+        public DeviceStateChangedEventArgs(string state, string command)
+        {
+            State = state;
+            Command = command;
+        }
+
+        public virtual string State { get; private set; }
+        public virtual string Command { get; private set; }
+    }
+
+    public delegate void DeviceStateChangedEventHandler(object sender, DeviceStateChangedEventArgs e);
+
+}
---
+++ HelloWorld/HelloWpf/MainViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace HelloWpf
+{
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            CommandLog = new ObservableCollection<string>();
+            Device = new MainDevice();
+            Device.OnStateChanged += Device_OnStateChanged;
+
+            DeviceTestCommand = new RelayCommand(DoTestDevice, CanTestDevice);
+        }
+
+        private bool CanTestDevice(object obj)
+        {
+            return true;
+        }
+
+        private void DoTestDevice(object obj)
+        {
+            Device.ExecuteCommand("TEST");
+        }
+
+        void Device_OnStateChanged(object sender, DeviceStateChangedEventArgs e)
+        {
+            CommandLog.Add(string.Format("{0:H:mm:ss.fff}: {1}-{2}", DateTime.Now, e.Command, e.State));
+        }
+        
+        /// <summary>
+        /// Журнал комманд
+        /// </summary>
+        public ICollection<string> CommandLog { get; private set; }
+
+        /// <summary>
+        /// Устройство
+        /// </summary>
+        public MainDevice Device { get; private set; }
+
+        public RelayCommand DeviceTestCommand { get; set; }
+    }
+}
