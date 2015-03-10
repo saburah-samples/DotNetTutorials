ee8f5478a08673f996bb78a7c5ef9baa709fe5cb
Denis_Vakhrushev <saburah2@gmail.com>
2015-03-10 16:37:46 +0400

    Walkthrough: Creating Your First Touch Application

---
+++ HelloWorld/BasicManipulation/ReadMe.txt
@@ -0,0 +1,2 @@
+﻿Walkthrough: Creating Your First Touch Application
+https://msdn.microsoft.com/en-us/library/ee649090(v=vs.100).aspx
--- HelloWorld/HelloWpf/DeviceButton.xaml.cs
+++ HelloWorld/HelloWpf/DeviceButton.xaml.cs
@@ -20,9 +20,98 @@ namespace HelloWpf
     /// </summary>
     public partial class DeviceButton : UserControl
     {
+        // Using a DependencyProperty as the backing store for PressCommand.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PressCommandProperty =
+            DependencyProperty.Register("PressCommand", typeof(ICommand), typeof(DeviceButton), new PropertyMetadata(null));
+
+        // Using a DependencyProperty as the backing store for PressCommandParameter.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PressCommandParameterProperty =
+            DependencyProperty.Register("PressCommandParameter", typeof(object), typeof(DeviceButton), new PropertyMetadata(0));
+
+        // Using a DependencyProperty as the backing store for ReleaseCommand.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ReleaseCommandProperty =
+            DependencyProperty.Register("ReleaseCommand", typeof(ICommand), typeof(DeviceButton), new PropertyMetadata(null));
+
+        // Using a DependencyProperty as the backing store for ReleaseCommandParameter.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ReleaseCommandParameterProperty =
+            DependencyProperty.Register("ReleaseCommandParameter", typeof(object), typeof(DeviceButton), new PropertyMetadata(0));
+
         public DeviceButton()
         {
             InitializeComponent();
+        }
+
+        protected override void OnPreviewMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnPreviewMouseLeftButtonDown(e);
+            IsPressCommandHandled = false;
+        }
+
+        protected override void OnPreviewMouseMove(MouseEventArgs e)
+        {
+            base.OnPreviewMouseMove(e);
+            if (e.LeftButton == MouseButtonState.Pressed)
+                ExecutePressCommand();
+        }
+
+        protected override void OnPreviewMouseLeftButtonUp(MouseButtonEventArgs e)
+        {
+            base.OnPreviewMouseUp(e);
+            ExecuteReleaseCommand();
+            IsPressCommandHandled = false;
+        }
+
+        private void ExecutePressCommand()
+        {
+            if (!IsPressCommandHandled && CanExecutePressCommand())
+            {
+                PressCommand.Execute(PressCommandParameter);
+                IsPressCommandHandled = true;
+            }
+        }
+
+        private bool CanExecutePressCommand()
+        {
+            return PressCommand != null && PressCommand.CanExecute(PressCommandParameter);
+        }
+
+        private void ExecuteReleaseCommand()
+        {
+            if (CanExecuteReleaseCommand())
+            {
+                ReleaseCommand.Execute(ReleaseCommandParameter);
+            }
+        }
+
+        private bool CanExecuteReleaseCommand()
+        {
+            return ReleaseCommand != null && ReleaseCommand.CanExecute(ReleaseCommandParameter);
+        }
+
+        public bool IsPressCommandHandled { get; private set; }
+
+        public ICommand PressCommand
+        {
+            get { return (ICommand)GetValue(PressCommandProperty); }
+            set { SetValue(PressCommandProperty, value); }
+        }
+
+        public object PressCommandParameter
+        {
+            get { return (object)GetValue(PressCommandParameterProperty); }
+            set { SetValue(PressCommandParameterProperty, value); }
+        }
+
+        public ICommand ReleaseCommand
+        {
+            get { return (ICommand)GetValue(ReleaseCommandProperty); }
+            set { SetValue(ReleaseCommandProperty, value); }
+        }
+
+        public object ReleaseCommandParameter
+        {
+            get { return (object)GetValue(ReleaseCommandParameterProperty); }
+            set { SetValue(ReleaseCommandParameterProperty, value); }
         }
     }
 }
