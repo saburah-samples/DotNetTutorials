f50aa965c3120a351a5589b468706a4e2228fb5f
Denis_Vakhrushev <saburah2@gmail.com>
2015-03-26 19:22:59 +0400

    HelloWorld.HelloWpf - added App.InstallUpdateSyncWithInfo using System.Deployment.Application.ApplicationDeployment

--- HelloWorld/HelloWpf/App.xaml.cs
+++ HelloWorld/HelloWpf/App.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Deployment.Application;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,5 +14,75 @@ namespace HelloWpf
     /// </summary>
     public partial class App : Application
     {
+        private void InstallUpdateSyncWithInfo()
+        {
+            UpdateCheckInfo info = null;
+
+            if (ApplicationDeployment.IsNetworkDeployed)
+            {
+                ApplicationDeployment ad = ApplicationDeployment.CurrentDeployment;
+                try
+                {
+                    //here as parameter we had provided false
+                    //because every time starts up application it will check for updates
+                    //and will not store the previous check result into memory.
+                    info = ad.CheckForDetailedUpdate(false);
+                }
+                catch (DeploymentDownloadException dde)
+                {
+                    MessageBox.Show("The new version of the application cannot be downloaded at this time. \n\nPlease check your network connection, or try again later. Error: " + dde.Message);
+                    return;
+                }
+                catch (InvalidDeploymentException ide)
+                {
+                    MessageBox.Show("Cannot check for a new version of the application. The ClickOnce deployment is corrupt. Please redeploy the application and try again. Error: " + ide.Message);
+                    return;
+                }
+                catch (InvalidOperationException ioe)
+                {
+                    MessageBox.Show("This application cannot be updated. It is likely not a ClickOnce application. Error: " + ioe.Message);
+                    return;
+                }
+
+                if (info.UpdateAvailable)
+                {
+                    Boolean doUpdate = true;
+
+                    if (!info.IsUpdateRequired)
+                    {
+                        MessageBoxResult dr = MessageBox.Show("An update is available. Would you like to update the application now?", "EasyLink - Update Available", MessageBoxButton.OKCancel);
+                        if (!(MessageBoxResult.OK == dr))
+                        {
+                            doUpdate = false;
+                        }
+                    }
+                    else
+                    {
+                        // Display a message that the app MUST reboot. Display the minimum required version.
+                        MessageBox.Show("This application has detected a mandatory update from your current " +
+                            "version to version " + info.MinimumRequiredVersion.ToString() +
+                            ". The application will now install the update and restart.",
+                            "Update Available", MessageBoxButton.OK,
+                            MessageBoxImage.Information);
+                    }
+
+                    if (doUpdate)
+                    {
+                        try
+                        {
+                            MessageBox.Show("The application will update in background. Please click on OK and wait for a moment...", "Background Update");
+                            ad.Update();
+                            MessageBox.Show("The application has been upgraded to the Newer Version (v" + ad.UpdatedVersion.ToString() + "). Please restart the application to run the New Version.", "Update Successful");
+                            Application.Current.Shutdown();
+                        }
+                        catch (DeploymentDownloadException dde)
+                        {
+                            MessageBox.Show("Cannot install the latest version of the application. \n\nPlease check your network connection, or try again later. Error: " + dde);
+                            return;
+                        }
+                    }
+                }
+            }
+        }
     }
 }
