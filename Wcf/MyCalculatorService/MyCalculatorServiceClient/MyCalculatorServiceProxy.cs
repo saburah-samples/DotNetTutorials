e4628900024fd7d0d5c327d507513d86b249cdf1
Denis Vakhrushev <saburah2@gmail.com>
2015-09-25 11:02:53 +0400

    Initial self-hosted MyCalculatorService by WCF

---
+++ Wcf/MyCalculatorService/MyCalculatorServiceClient/MyCalculatorServiceProxy.cs
@@ -0,0 +1,18 @@
+﻿using MyCalculatorService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+
+namespace MyCalculatorServiceClient
+{
+	class MyCalculatorServiceProxy : ClientBase<ISimpleCalculator>, ISimpleCalculator
+	{
+		public int Add(int num1, int num2)
+		{
+			//Call base to do funtion
+			return base.Channel.Add(num1, num2);
+		}
+	}
+}
---
+++ Wcf/MyCalculatorService/MyCalculatorServiceClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyCalculatorServiceClient
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			MyCalculatorServiceProxy proxy;
+			proxy = new MyCalculatorServiceProxy();
+			Console.WriteLine("Client is running at " + DateTime.Now.ToString());
+			Console.WriteLine("Sum of two numbers... 5+5 =" + proxy.Add(5, 5));
+			Console.ReadLine();
+		}
+	}
+}
